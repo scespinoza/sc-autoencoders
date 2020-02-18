617bc026d835301801b37955ca834e8175bd3eca
EC2 Default User <ec2-user@ip-172-31-34-182.ec2.internal>
2020-02-18 17:51:05 +0000

    remove train

--- train.py
+++
@@ -1,7 +0,0 @@
-from 
-from preprocess import GSE
-from models import AutoEncoder
-
-if __name__ == '__main__':
-    dataset = GSE(name='GSE57872')
-    model = AutoEncoder(original_dim=dataset.n_genes)
