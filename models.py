76c8353c81f7a753b10c6e50b4c1ff15efb76864
Sebastian <secancinoe@gmail.com>
2020-02-19 09:32:43 -0300

    debugg

--- models.py
+++ models.py
@@ -280,7 +280,7 @@ class PrintLossAndAccuracy(tf.keras.callbacks.Callback):
         loss = logs['loss']
         val_loss = logs['val_loss']
         print('Epoch: {}, loss: {:.2f}, val_loss: {:.2f}, Acc: {:.2f}'.format(epoch, loss, val_loss, acc))
-        z = self.model.encode(x[:1])
+        z = self.model.encode(self.x[:1])
         print(z)
         gamma = self.model.compute_gamma(z)
         print(gamma)
