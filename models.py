c96fa38282d4f7086c7b7ea4f121ee0a5b81a8c3
Sebastian <secancinoe@gmail.com>
2020-02-19 11:13:51 -0300

    debug

--- models.py
+++ models.py
@@ -282,11 +282,8 @@ class PrintLossAndAccuracy(tf.keras.callbacks.Callback):
         print('Epoch: {}, loss: {:.2f}, val_loss: {:.2f}, Acc: {:.2f}'.format(epoch, loss, val_loss, acc))
 
         # debugg gamma
-        z = self.model.encode(self.x[:1])
-        #print('z: ', z.numpy())
-        gamma = self.model.compute_gamma(z)
 
-        print('gamma: ', gamma.numpy())
+        print('pi: ', self.model.pi_prior.numpy())
         """
         h = (z[:, np.newaxis, :] - self.model.mu_prior.numpy()) ** 2 / np.exp(self.model.logvar_prior.numpy())
         h += self.model.logvar_prior.numpy()
