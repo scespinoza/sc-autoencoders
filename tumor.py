7bf5ac81941faf0103463af5fcbcc7b0d87f7afb
Sebastian <secancinoe@gmail.com>
2020-02-20 16:01:48 -0300

    tumor

--- tumor.py
+++ tumor.py
@@ -2,6 +2,7 @@ import pandas as pd
 from tensorflow.keras import losses
 from tensorflow.keras import callbacks
 from tensorflow.keras import optimizers
+from sklearn.preprocessing import LabelEncoder, MinMaxScaler
 from models import * 
 
 if __name__ == '__main__':
@@ -9,14 +10,18 @@ if __name__ == '__main__':
     n_cells, n_genes = data.shape
     name='stacked_mgh26'
 
-    early_stopping = callbacks.EarlyStopping(patience=args.patience)
+    data_scaled = MinMaxScaler().fit_transform(data.values)
+    early_stopping = callbacks.EarlyStopping(patience=50)
     model_checkpoint = callbacks.ModelCheckpoint('weights/' + name + '_trained.h5',
                                                  save_best_only=True,
                                                  save_weights_only=True)
+
+    metadata = pd.read_csv('data/GSE57872_metadata.txt')
+    classes = LabelEncoder().fit_transform(metadata[metadata['patient_id'] == 'MGH26']['subtype'])
     
-    plot_latent = PlotLatentSpace(model, dataset.data_scaled, dataset.class_labels, interval=args.interval)
+    plot_latent = PlotLatentSpace(model, data_scaled, classes, interval=20)
 
     model = AutoEncoder(original_dim=n_genes, name=name)
     model.compile(optimizer=optimizers.Adam(0.0001), loss='binary_crossentropy')
-    model.fit(data.values, data.values, epochs=1000, validation_data=(data.values, data.values),
+    model.fit(data_scaled, data_scaled, epochs=1000, validation_data=(data_scaled, data_scaled),
              callbacks=[early_stopping, plot_latent, model_checkpoint])
